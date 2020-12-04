bfc72ddaff200c0504896f683c118a0fc2f6633b
Atanu Barai <atanu.bct@gmail.com>
2020-12-03 21:40:37 -0700

    added .vscode in gitignore and chaged variable name in addr2cline.py

--- addr2cacheline.py
+++ addr2cacheline.py
@@ -1,6 +1,6 @@
 # Author: Atanu Barai <atanu@nmsu.edu>
 # Date: 13 September 2020
-# Purpose: Replaces basic block random numbers with basic block names, reports any overlap of basic 
+# Purpose: Replaces basic block random numbers with basic block names, reports any overlap of basic
 # blocks
 # Copyright: PEARL Laboratory, ECE, NMSU. Part of the PPT open source project.
 # Python3 Version, used pandas to work with large trace files
@@ -8,9 +8,10 @@
 import sys
 import math
 
-CACHE_LINE_SIZE  = 64
+CACHE_LINE_SIZE = 64
 
 rshift = int(math.log2(CACHE_LINE_SIZE))
+
 
 def main(bb_file, trace_file):
     '''
@@ -24,11 +25,11 @@ def main(bb_file, trace_file):
         if 'shared_trace' in b_t_info[2]:
             shared_bbs.append(b_t_info[1])
     count = 0
-    fp = open(trace_file, 'r')
+    file_p = open(trace_file, 'r')
     o_t_file = open('processed_trace.dat', 'w')
     while True:
         count += 1
-        line = fp.readline()
+        line = file_p.readline()
         if not line:
             break
         else:
@@ -36,9 +37,9 @@ def main(bb_file, trace_file):
                 addr = int(line, 0)
                 addr = addr >> rshift
                 o_t_file.write(line)
-    fp.close()
+    file_p.close()
     o_t_file.close()
+
+
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
-
-
