e9940ff4f926c48ba76d410f84b8ea40c35dfedf
Atanu Barai <atanu.bct@gmail.com>
2020-12-07 17:26:13 -0700

    Now traceInterleaver.c takes only number of cores as the argument

--- traceInterleaver.c
+++ traceInterleaver.c
@@ -1,77 +1,85 @@
 // Author: Atanu Barai <atanu@nmsu.edu>
 // Purpose: Interleaves memory trace
-
+// Last Edit Nov 26
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define CACHE_LINE_SIZE 64 //Cache block/line size in bytes 
 
 #define MAX_CORES 16
 
 int main(int argc, char *argv[])
 {
-    char * line = NULL;
+    char *line = NULL;
     size_t len = 0;
     ssize_t read;
-    unsigned long long trace_len = 0, i = 0, trace_len_per_core, number;
+    unsigned long long i = 0, trace_trav_done[MAX_CORES];
     int num_cores, core;
     FILE *outfile;
     FILE *infile[MAX_CORES];
-    int rshift = log2(CACHE_LINE_SIZE);
-    char str[30];
+    char str[40];
 
-    if(argc != 3)
+    if (argc != 2)
     {
-        printf("%d of 2 arguments received\n", (argc-1));
-        printf("Command Line Arguments: original trace length, number of cores\n");
+        printf("%d of 1 arguments received\n", (argc - 1));
+        printf("Command Line Arguments: number of cores\n");
         exit(1);
     }
     else
     {
-        printf("%d of 2 arguments received\n", (argc-1));
-        trace_len = atoll(argv[1]);
-        num_cores = atoi(argv[2]);
-        printf("Trace Length: %llu, Number of Cores: %d\n", trace_len, num_cores);
+        printf("%d of 1 arguments received\n", (argc - 1));
+        num_cores = atoi(argv[1]);
+        printf("Number of Cores: %d\n", num_cores);
     }
 
-    for(core = 0; core < num_cores; core++)
+    for (core = 0; core < num_cores; core++)
     {
-        sprintf(str, "%dcores_trace_core%d.dat",num_cores, core);
-        infile[core] = fopen(str,"r");
-        if(infile[core] == NULL)
+        trace_trav_done[core] = 0;
+        sprintf(str, "%dcores_trace_core%d.dat", num_cores, core);
+        //printf("%s\n",str);
+        infile[core] = fopen(str, "r");
+        if (infile[core] == NULL)
         {
-            printf("Error in opening input file on %dth time!\n", core+1);
+            printf("Error in opening input file on %dth time!\n", core + 1);
             exit(1);
         }
     }
-    sprintf(str, "InterleavedTrace%dCores.dat",num_cores);
-    outfile = fopen(str,"w");
-    if(outfile == NULL)
+    sprintf(str, "InterleavedTrace%dCores.dat", num_cores);
+    outfile = fopen(str, "w");
+    if (outfile == NULL)
     {
         printf("Error in opening input file \n");
         exit(1);
     }
 
-    trace_len_per_core = trace_len / num_cores;
     printf("Interleaving Start\n");
-    while(i < trace_len_per_core)
+    i = 0;
+    while (i < num_cores)
     {
-        for(core = 0; core < num_cores; core++)
+        for (core = 0; core < num_cores; core++)
         {
-            if((read = getline(&line, &len, infile[core])) != -1)
+            if ((read = getline(&line, &len, infile[core])) != -1)
+            {
                 fprintf(outfile, "%s", line);
+                //printf("%s", line);
+            }
+            else
+            {
+                if (trace_trav_done[core] == 0)
+                {
+                    trace_trav_done[core] = 1;
+                    ++i;
+                }
+            }
         }
-        ++i;
-    } 
+    }
     printf("Interleaving Done. Releasing Resources\n");
     if (line)
         free(line);
     fclose(outfile);
-    for(core = 0; core < num_cores; core++)
+    for (core = 0; core < num_cores; core++)
         fclose(infile[core]);
     printf("Resources Released\n");
     return 0;
 }
-
